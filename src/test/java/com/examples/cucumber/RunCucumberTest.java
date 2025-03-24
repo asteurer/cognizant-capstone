6d458975bbd4d10db94fe94b9d50ee55c4e37b25
Carson <carson.freeman@cognizant.com>
2025-03-24 13:44:30 -0600

    Added setup and teardown methods for a proper test functionality, may need to be edited in the future when combined with cucumber testing

--- src/test/java/com/examples/cucumber/RunCucumberTest.java
+++ src/test/java/com/examples/cucumber/RunCucumberTest.java
@@ -1,12 +1,50 @@
 package com.examples.cucumber;
 
+import com.youtube.pageobjects.YouTubeVideoPage;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"classpath:features/MyBeerCans.feature"},
         glue = {"com.examples.cucumber"})
 public class RunCucumberTest {
+    private WebDriver driver;
+    private Logger logger;
+    @BeforeMethod(alwaysRun = true)
+    @Parameters("browser")
+    public void setUp(@Optional("chrome") String browser) {
+        logger = Logger.getLogger(RunCucumberTest.class.getName());
+        logger.setLevel(Level.INFO);
+        logger.info("Running test in " + browser);
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            default:
+                logger.warning("Configuration for " + browser + " is missing, so running tests in Chrome by default");
+                driver = new ChromeDriver();
+                break;
+        }
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(){
+        driver.quit();
+        logger.info("Browser is closed");
+    }
+
+
 }
