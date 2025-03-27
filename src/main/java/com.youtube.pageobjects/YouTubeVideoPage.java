fb1a78270463b57967ffd81b7b2d93afa6b9b708
Ryan <drockstarr33@yahoo.com>
2025-03-27 12:34:47 -0500

    Added negative video testing; 1 feature unfinished

--- src/main/java/com.youtube.pageobjects/YouTubeVideoPage.java
+++ src/main/java/com.youtube.pageobjects/YouTubeVideoPage.java
@@ -2,6 +2,7 @@ package com.youtube.pageobjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 public class YouTubeVideoPage extends BasePage{
     //Locator for embed text
@@ -14,9 +15,25 @@ public class YouTubeVideoPage extends BasePage{
     private By descriptionDropDownLocator = By.xpath("//*[@id='expand']");
     //Locator for date once dropdown is executed
     private By datePostedLocator = By.xpath("//*[@id='info']/span[3]");
+    //Locator for sign-in button
+    private By loginButtonLocator = By.xpath("//span[text()='Sign in']/../../..");
+    //Locator for logged out menu
+    private By loggedOutSettingsMenuLocator = By.xpath("//yt-icon[@class='style-scope ytd-topbar-menu-button-renderer']");
+    //Locator for account menu
+    private By loggedInSettingsMenuLocator = By.xpath("//button[@aria-label='Account menu']");
+    //Locator for sign out button
+    private By signOutSettingsOptionLocator = By.xpath("//yt-formatted-string[text()='Sign out']");
+    //Locator for settings button
+    private By settingsSettingsOptionLocator = By.xpath("//yt-formatted-string[text()='Settings']");
+    //Locator for subscribe button
+    private By subscribeButtonLocator = By.xpath("//*[@id='subscribe-button-shape']/button");
+    //Locator for subscribe pop up when not signed in
+    private By subscribePopUpTextLocator = By.xpath("//yt-formatted-string[text()='Sign in to subscribe to this channel.']");
+
     public YouTubeVideoPage(WebDriver driver) {
         super(driver);
     }
+
     public void visit() {
         super.visit("https://www.youtube.com/watch?v=lC0jzd8sGIA");
     }
@@ -37,5 +54,40 @@ public class YouTubeVideoPage extends BasePage{
 
     public void clickEmbed(WebDriver driver) {
         driver.findElement(embedButtonLocator).click();
+    }
+
+    public boolean isSignInButtonVisible() {
+        return waitForElement(loginButtonLocator).isDisplayed();
+    }
+
+    /**
+     * Clicks the settings menu. If the user is logged in, uses a separate locator
+     */
+    public void clickSettingsMenu() {
+        if(driver.findElement(loginButtonLocator).isDisplayed()) {
+            driver.findElement(loggedOutSettingsMenuLocator).click();
+        } else {
+            driver.findElement(loggedInSettingsMenuLocator).click();
+        }
+    }
+
+    public void clickSignOut() {
+        driver.findElement(signOutSettingsOptionLocator).click();
+    }
+
+    public void clickSubscribeButton() {
+        driver.findElement(subscribeButtonLocator).click();
+    }
+
+    public boolean isSubscriptionPopUpVisible() {
+        return driver.findElement(subscribePopUpTextLocator).isDisplayed();
+    }
+
+    public String getSubscriptionPopUpText() {
+        return driver.findElement(subscribePopUpTextLocator).getText();
+    }
+
+    public void clickSettingsButton() {
+        driver.findElement(settingsSettingsOptionLocator).click();
     }
 }
---
+++ src/test/java/com/examples/cucumber/MyStepdefs/NegativeVideoTestStepdefs.java
@@ -0,0 +1,148 @@
+package com.examples.cucumber.MyStepdefs;
+
+import com.youtube.pageobjects.YouTubeVideoPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class NegativeVideoTestStepdefs {
+
+    private WebDriver driver;
+    private Logger logger;
+
+    /**
+     * Setup function for our browser and logger. Note that depending on the setup of our Cucumber tests, it may be
+     * easiest to use this without annotations.
+     *
+     * @param browser
+     */
+    @BeforeClass(alwaysRun = true)
+    @Parameters("browser")
+    public void setup(@Optional("chrome") String browser) {
+
+        logger = Logger.getLogger(YouTubeVideoPage.class.getName());
+        logger.setLevel(Level.INFO);
+
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                logger.info("Testing with " + browser + " browser");
+                driver = new ChromeDriver();
+                break;
+            default:
+                logger.warning("Configuration missing for browser \"" + browser + "\", defaulting to Chrome.");
+                driver = new ChromeDriver();
+                break;
+        }
+    }
+
+    /**
+     * Teardown function for our browser.
+     */
+    @AfterClass(alwaysRun = true)
+    public void teardown() {
+        driver.quit();
+        logger.info("Closed browser");
+    }
+
+    /**
+     * Negative tests
+     * <p>
+     * Test 1: Subscribing while signed out
+     * <p>
+     * Given I am on a YouTube video page in the Firefox browser
+     * And I am not signed in
+     * When I click the subscribe button
+     * Then I should see a sign-in subscription pop-up
+     * <p>
+     * Test 2: Opening settings menu while signed out
+     * <p>
+     * Given I am on a YouTube video page in the Firefox browser
+     * And I am not signed in
+     * When I open the settings menu and click the Settings button
+     * Then I should be taken to the Google sign-in page
+     * <p>
+     * Test 3: Saving while signed out??
+     */
+
+    @Given("I am on a YouTube video page in the {word} browser")
+    public void on_youtube_video_page(String browser) {
+        setup(browser);
+
+        logger.info("Opening page");
+        driver.get("https://www.youtube.com/watch?v=P7gt3g7iq7M");
+    }
+
+    /**
+     * This step ensures that we are not signed in; if we are, sign out.
+     *
+     * TODO: Was unable to test sign-out functionality since Google will not let me sign in with automation software running.
+     */
+    @Given("I am not signed in")
+    public void not_signed_in() {
+        YouTubeVideoPage videoPage = new YouTubeVideoPage(driver);
+
+        logger.info("Ensure that Sign In button is visible in top right");
+        if(!videoPage.isSignInButtonVisible()) {
+            logger.info("Sign In button is not visible; logging out");
+
+            logger.info("If settings menu is visible, open it");
+            videoPage.clickSettingsMenu();
+
+            logger.info("Click sign out button");
+            videoPage.clickSignOut();
+        }
+    }
+
+    @When("I click the subscribe button")
+    public void click_the_subscribe_button() {
+        YouTubeVideoPage videoPage = new YouTubeVideoPage(driver);
+
+        logger.info("Click the subscribe button");
+        videoPage.clickSubscribeButton();
+    }
+
+    @Then("I should see a sign-in subscription pop-up")
+    public void verify_sign_in_subscription_pop_up() {
+        YouTubeVideoPage videoPage = new YouTubeVideoPage(driver);
+
+        logger.info("Assert that the YouTube subscription pop-up appears");
+        Assert.assertTrue(videoPage.isSubscriptionPopUpVisible());
+
+        logger.info("Verify that pop-up has the correct text");
+        Assert.assertEquals("Sign in to subscribe to this channel.", videoPage.getSubscriptionPopUpText());
+    }
+
+    @When("I open the settings menu")
+    public void open_settings_menu() {
+        YouTubeVideoPage videoPage = new YouTubeVideoPage(driver);
+
+        logger.info("Opening the settings menu");
+        videoPage.clickSettingsMenu();
+    }
+
+    @When("I click the settings button")
+    public void click_settings_button() {
+        YouTubeVideoPage videoPage = new YouTubeVideoPage(driver);
+
+        logger.info("Clicking the settings button in the settings menu");
+        videoPage.clickSettingsButton();
+    }
+
+    @Then("I should be taken to the Google sign-in page")
+    public void verify_google_sign_in_page() {
+        YouTubeVideoPage videoPage = new YouTubeVideoPage(driver);
+
+        logger.info("Verifying that we are taken to the sign-in page");
+        //TODO
+    }
+}
